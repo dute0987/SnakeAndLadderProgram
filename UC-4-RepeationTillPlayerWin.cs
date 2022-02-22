63bb4cca451e836441853c63b6cab6713d28ea2f
dute0987 <dute0987@gmail.com>
2022-02-22 20:45:23 +0530

    file for getting winning position of 100

---
+++ UC-4-RepeationTillPlayerWin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4_SnakeAndLadderProblem
+{
+    public class UC_4_RepeationTillPlayerWin
+    {
+        /// <summary>
+        /// UC-4-Repeations the till winning position.
+        /// </summary>
+        public static void RepeationTillWinningPosition()
+        { 
+
+         int START = 0;
+         const int NOPLAY = 1;
+         const int SNAKE = 2;
+         const int LADDER = 3;
+         int position = 0;
+         int dice, option;
+   
+         Random random = new Random();
+            
+            while (position <= 100)
+            {
+                dice = random.Next(1, 7);
+                option = random.Next(1, 4);
+                if (position< 0)
+                {
+                    position = 0;
+                }
+                switch (option)
+                {
+                    case NOPLAY:
+                        position += 0;
+                        break;
+                    case SNAKE:
+                        position -= dice;
+                        break;
+                    case LADDER:
+                        position += dice;
+                        break;
+                }
+                Console.WriteLine(position);
+            }
+        }
+    }
+}
