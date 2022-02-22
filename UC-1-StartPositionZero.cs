8235716d4a703a245c60f20e2e47d8325f007839
dute0987 <dute0987@gmail.com>
2022-02-22 20:24:10 +0530

    file of start position from zero

---
+++ UC-1-StartPositionZero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4_SnakeAndLadderProblem
+{
+    public class UC_1_StartPositionZero
+    {
+        /// <summary>
+        /// UC-1-Snakes ladder program for zero position.
+        /// </summary>
+        public static void SnakeLadderProgramFromZeroPosition()
+        {
+            int Start = 0;
+            int Position = 0;
+            Console.WriteLine(Position);
+
+        }
+    }
+
+}
