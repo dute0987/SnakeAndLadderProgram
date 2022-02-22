2539d7f17849251e0bacd088f866794cb184039b
dute0987 <dute0987@gmail.com>
2022-02-22 20:32:10 +0530

    file of generate random number for dice number

---
+++ UC-2-RollDiceForNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4_SnakeAndLadderProblem
+{
+    public class UC_2_RollDiceForNumber
+    {
+        /// <summary>
+        /// UC-2-Rolls the die to get value.
+        /// </summary>
+        public static void RollDieToGetValue()
+        {
+            int START = 0;
+            int POSITION = 0;
+            
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+
+            Console.WriteLine(dice);
+        }
+    }
+}
