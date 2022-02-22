fd2e64ed14bb7f21fd095a4c374a60297d585e75
dute0987 <dute0987@gmail.com>
2022-02-22 20:42:43 +0530

    program.cs for Repeated file of winning position

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace Day_4_SnakeAndLadderProblem
             //Console.WriteLine("Welcome To the Snake and Ladder Program");
             //UC_1_StartPositionZero.SnakeLadderProgramFromZeroPosition();
             //UC_2_RollDiceForNumber.RollDieToGetValue();
-              UC_3_CheckAndPlayForOption.PlayerCheckOptionToplay();
-            //UC_4_RepeationTillPlayerWin.RepeationTillWinningPosition();
+            //UC_3_CheckAndPlayForOption.PlayerCheckOptionToplay();
+              UC_4_RepeationTillPlayerWin.RepeationTillWinningPosition();
             //UC_5_PlayerGetExactWinnigPosition.ExactWinningPositionByPlayer();
             //UC_6_DiePositionUntilWin.PlayTheDiceAndShowPosition();
             //UC_7_PlayGameUsingTwoPlayer.PlayGameShowPlayerWhoWon();
