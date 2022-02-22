2356de6b3e1ee3ad1eae3e8090c0c42dea3e9421
dute0987 <dute0987@gmail.com>
2022-02-22 20:09:12 +0530

    greeting message for Snake and ladder program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Day_4_SnakeAndLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+              Console.WriteLine("Welcome To the Snake and Ladder Program");
+            //UC_1_StartPositionZero.SnakeLadderProgramFromZeroPosition();
+            //UC_2_RollDiceForNumber.RollDieToGetValue();
+            //UC_3_CheckAndPlayForOption.PlayerCheckOptionToplay();
+            //UC_4_RepeationTillPlayerWin.RepeationTillWinningPosition();
+            //UC_5_PlayerGetExactWinnigPosition.ExactWinningPositionByPlayer();
+            //UC_6_DiePositionUntilWin.PlayTheDiceAndShowPosition();
+            //UC_7_PlayGameUsingTwoPlayer.PlayGameShowPlayerWhoWon();
+        }
+    }
+}
